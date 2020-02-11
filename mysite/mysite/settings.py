942860d7e50f991142d08a17cc7bbd103004b0ba
jtkim03 <jtk2rw@virginia.edu>
2020-02-11 01:39:02 -0500

    established homepage

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'find_a_qt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-def home_view(request):
+def home(request):
     template = "homepage.html"
     context = {}
     return render(request,template,context)
