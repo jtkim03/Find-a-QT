5857baf23146061d0183fd5c79066ba2d7fafa2d
Karan Goyal <kg3be@virginia.edu>
2020-02-17 12:06:48 -0500

    static

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = 'afewbeguta=wh-3)4717_s$vn&1xl4#^67dckf+f4oxb=p(_m3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 ALLOWED_HOSTS = []
 
 
