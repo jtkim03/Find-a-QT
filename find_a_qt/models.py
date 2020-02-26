b474aeb17b2b1f85d12187894910879382a79655
DanNguyen-Huu <dhn8ca@virginia.edu>
2020-02-26 03:24:18 -0500

    db and student model

--- find_a_qt/models.py
+++ find_a_qt/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 # hello world
 # Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+class Student(models.Model):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    GRADUATE = 'GR'
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+        (GRADUATE, 'Graduate'),
+    ]
+    year_in_school = models.CharField(
+        max_length=2,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default=FRESHMAN,
+    )
+
+    major = models.CharField(max_length=50)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    name = models.CharField(max_length=50)
