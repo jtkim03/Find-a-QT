a6f9bab2f252e3df2758717c4e8676c573064243
DanNguyen-Huu <dhn8ca@virginia.edu>
2020-02-27 15:06:12 -0500

    update db settings and made migrations

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'find_a_qt',
-        'USER': 'FindAQT',
+        'NAME': 'FindAQT',
+        'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
     }
