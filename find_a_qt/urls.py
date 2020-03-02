bd44d0ed043d6de2078fe42d7db4c2d8eeef1c6f
karangyl <kg3be@virginia.edu>
2020-03-01 19:24:25 -0500

    Minor tweaks

--- find_a_qt/urls.py
+++ find_a_qt/urls.py
@@ -6,4 +6,7 @@ from django.contrib.staticfiles.URLs import staticfiles_urlpatterns
 urlpatterns = [
     path('', views.home_view, name='index'),
     path('about', views.about_view, name = 'about')
+    path('studentregister', views.student_register, name = 'studentregister')
+    path('tutorregister', views.tutor_register, name = 'tutorregister')
+
 ]
