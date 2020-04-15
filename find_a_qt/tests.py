6c92496eb1ac499bdfaf429c7a8667445541deb5
karangyl <kg3be@virginia.edu>
2020-04-15 10:32:16 -0400

    Added tests

--- find_a_qt/tests.py
+++ find_a_qt/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
-from .models import Question
+from .models import Question, Answer
 from users.models import Profile
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.db import IntegrityError
 from django.conf import settings
+from .forms import QuestionForm, AnswerForm, RoomForm
+from chat.models import Room
 import os 
+
 
 class QuestionStrTestCase(TestCase):
     def setUp(self):
@@ -25,6 +28,7 @@ class QuestionPhotoTestCase(TestCase):
         self.assertEqual(Question.objects.count(), 1)
 '''
 
+
 class QuestionIndexTest(TestCase):
     def setUp(self):
         Question.objects.create(body="What is life?", class_name ="PHIL 1710", author_name = 'Josh', topic = 'CS')
@@ -37,6 +41,7 @@ class QuestionIndexTest(TestCase):
         self.assertContains(response, 'What is 9+10?')
         self.assertContains(response, 'How do I do 3240?')
 
+
 class ProfileStrTestCase(TestCase):
     def setUp(self):
         self.test_user = User.objects.create_user('JohnD', 'johnd@mail.com', 'johnpassword')
@@ -45,6 +50,7 @@ class ProfileStrTestCase(TestCase):
         test_user = self.test_user
         test_profile = Profile.objects.get(user=test_user)
         self.assertEqual(str(test_profile), "JohnD's Profile")
+
 
 class UserCreationFailCase(TestCase):
     def setUp(self):
@@ -55,3 +61,55 @@ class UserCreationFailCase(TestCase):
             self.test_user2 = User.objects.create_user('JohnD', 'johnd@mail.com', 'johnpassword')
         except:
             self.assertRaises(AttributeError)
+
+
+class AnswerStrTestCase(TestCase):
+    def setUp(self):
+        Question.objects.create(body="What is life?", class_name ="PHIL 1710", author_name = 'Josh', topic = 'CS')
+        Question.objects.create(body="What is 9+10?", class_name ="MATH 101", author_name = 'Drake', topic = 'MATH')
+        Question.objects.create(body="How do I do 3240?", class_name ="CS 3240", author_name = 'FindaQT', topic = 'CS')
+        Answer.objects.create(text ="Life is fun", post_id = 1)
+        Answer.objects.create(text ="9+10 = 19", post_id = 1)
+        Answer.objects.create(text ="You just use Django and start coding buddy", post_id = 1)
+
+    def test_str(self):
+        response = self.client.get('/answers/')
+        self.assertContains(response, 'Life is fun')
+        self.assertContains(response, '9+10 = 19')
+        self.assertContains(response, 'You just use Django and start coding buddy')
+
+
+class QuestionFormTest(TestCase):
+    def setUp(self):
+        q1 = Question.objects.create(body="What is life?", class_name ="PHIL 1710", author_name = 'Josh', topic = 'Computer Science', urgency = 'No rush')
+
+    def test_form_true(self):
+        w = Question.objects.get(body="What is life?")
+        data = {'body': w.body, 'class_name': w.class_name, 'class_name': w.class_name, 'author_name': w.author_name, 'topic': w.topic, 'urgency':w.urgency, 'session_date':w.session_date, 'session_time': w.session_time}
+        form = QuestionForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_form_false(self):
+        w = Question.objects.get(body="What is life?")
+        data = {'body': w.body, 'author_name': w.author_name, 'topic': w.topic, 'urgency':w.urgency, 'session_date':w.session_date, 'session_time': w.session_time}
+        form = QuestionForm(data=data)
+        self.assertFalse(form.is_valid())
+
+
+
+class RoomFormTest(TestCase):
+    def setUp(self):
+        r1 = Room.objects.create(name = 'STAT2120', slug ='STAT-2120' , description = "Chat for STAT 2120 students")
+
+    def test_form_true(self):
+        w = Room.objects.get(name="STAT2120")
+        data = {'name': w.name, 'slug': w.slug, 'description': w.description, }
+        form = RoomForm(data=data)
+        print(form)
+        self.assertTrue(form.is_valid())
+
+    def test_form_false(self):
+        w = Room.objects.get(name="STAT2120")
+        data = { 'slug': w.slug, 'description': w.description, }
+        form = RoomForm(data=data)
+        self.assertFalse(form.is_valid())
